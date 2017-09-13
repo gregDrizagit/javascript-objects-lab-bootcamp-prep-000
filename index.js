500b0c7cc3908fa32f00e2662bfff08a6291aebd
Greg Driza <gdriza@uarts.edu>
2017-09-13 00:13:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = new Object ({});
+
+function updateObjectWithKeyAndValue(object, key, value)
+{
+  object[key] = value;
+  return ;
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+
+
+}
+
+function deleteFromObjectByKey(object, key)
+{
+
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key)
+{
+
+}
