369a0a098772d951114632748ba8c6ed9ab318e8
Greg Driza <gdriza@uarts.edu>
2017-09-13 00:30:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var recipes = new Object ({});
 function updateObjectWithKeyAndValue(object, key, value)
 {
 
-  var obj = { [key]: [value]};
-
-  var newObj = Object.assign({}, obj)
-  return newObj;
+  //var obj = { [key]: [value]};
+  object[key] = value; 
+  //var newObj = Object.assign({}, obj)
+  return object;
 
 }
 
